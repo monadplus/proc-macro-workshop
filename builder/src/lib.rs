34280481c5d8737e14ef9a393f24c628b5779e3c
Arnau Abella <arnauabella@gmail.com>
2022-12-01 22:56:12 +0100

    builder/01-parse

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,22 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input: DeriveInput = parse_macro_input!(input as DeriveInput);
+    match input.data {
+        syn::Data::Struct(struk) => match struk.fields {
+            syn::Fields::Named(named) => {
+                for field in named.named {
+                    println!("{:?}", field.ty);
+                }
+            }
+            syn::Fields::Unnamed(_) => todo!(),
+            syn::Fields::Unit => todo!(),
+        },
 
-    unimplemented!()
+        syn::Data::Enum(_) => todo!(),
+        syn::Data::Union(_) => todo!(),
+    }
+    TokenStream::new()
 }
