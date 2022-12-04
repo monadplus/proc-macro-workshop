32789e14caf1d7908c272799f03cac016e66f15d
Arnau Abella <arnauabella@gmail.com>
2022-12-04 21:30:21 +0100

    seq/03-expand-four-errors

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,45 +1,79 @@
-use proc_macro::TokenStream;
+use proc_macro2::{TokenStream, TokenTree};
 use syn::{braced, parse::Parse, parse_macro_input, Token};
+
+#[proc_macro]
+pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let mut seq = parse_macro_input!(input as Sequence);
+    seq.expand().into()
+}
 
 struct Sequence {
     var_name: syn::Ident,
-    start: syn::LitInt,
-    end: syn::LitInt,
-    loop_expr: proc_macro2::TokenStream,
+    start: u64,
+    end: u64,
+    body: proc_macro2::TokenStream,
 }
 
+// ```rust, ignore
 // seq!(N in 0..8 {
 //   ...
 // });
+// ```
 impl Parse for Sequence {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let var_name: syn::Ident = input.parse()?;
         input.parse::<Token![in]>()?;
         let start: syn::LitInt = input.parse()?;
+        let start: u64 = start.base10_parse()?;
         input.parse::<Token![..]>()?;
         let end: syn::LitInt = input.parse()?;
+        let end: u64 = end.base10_parse()?;
         let content;
         braced!(content in input);
-        let loop_expr: proc_macro2::TokenStream = content.parse()?;
+        let body: proc_macro2::TokenStream = content.parse()?;
         Ok(Self {
             var_name,
             start,
             end,
-            loop_expr,
+            body,
         })
     }
 }
 
-#[proc_macro]
-pub fn seq(input: TokenStream) -> TokenStream {
-    let Sequence {
-        var_name,
-        start,
-        end,
-        loop_expr,
-    } = parse_macro_input!(input as Sequence);
+impl Sequence {
+    fn expand(&mut self) -> TokenStream {
+        (self.start..self.end).fold(TokenStream::new(), |mut ts, i| {
+            let ts_aux = self.replace_number(self.body.clone(), i);
+            ts.extend(ts_aux);
+            ts
+        })
+    }
 
-    let output = proc_macro2::TokenStream::new();
+    fn replace_number(&self, body: TokenStream, val: u64) -> TokenStream {
+        let mut output_stream = TokenStream::new();
+        let mut token_iter = body.into_iter();
 
-    TokenStream::from(output)
+        while let Some(token) = token_iter.next() {
+            let output_token = match token {
+                // N
+                TokenTree::Ident(ref ident) if ident == &self.var_name => {
+                    let mut lit = proc_macro2::Literal::u64_unsuffixed(val);
+                    lit.set_span(token.span());
+                    TokenTree::from(lit)
+                }
+                // Expand content of (), {}, []
+                TokenTree::Group(ref group) => {
+                    let del = group.delimiter();
+                    let stream = self.replace_number(group.stream(), val);
+                    let mut group = proc_macro2::Group::new(del, stream);
+                    group.set_span(token.span());
+                    TokenTree::from(group)
+                }
+                _ => token,
+            };
+            output_stream.extend(TokenStream::from(output_token));
+        }
+
+        output_stream
+    }
 }
