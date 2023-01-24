6cefa1ded36632f2b2a3ca2e4ce9002690a8cc37
Arnau Abella <arnauabella@gmail.com>
2023-01-24 09:08:39 +0100

    debug/01-parse

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let _ = parse_macro_input!(input as DeriveInput);
+    let output = TokenStream::new();
+    proc_macro::TokenStream::from(output)
 }
