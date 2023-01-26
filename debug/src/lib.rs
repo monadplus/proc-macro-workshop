739757ae60b4bb29b85f6e21440ddb846b1f4237
Arnau Abella <arnauabella@gmail.com>
2023-01-26 08:54:56 +0100

    debug/02-impl-debug

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,9 +1,44 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Fields};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
-    let output = TokenStream::new();
+    let DeriveInput {
+        attrs: _,
+        vis: _,
+        ident: struct_ident,
+        generics: _,
+        data,
+    } = parse_macro_input!(input as DeriveInput);
+
+    let fields = match data {
+        syn::Data::Struct(strct) => {
+            if let Fields::Named(fields) = strct.fields {
+                fields.named
+            } else {
+                unimplemented!("Builder only supports named fields")
+            }
+        }
+        other => unimplemented!("Builder is not supported for {:?}", other),
+    };
+
+    let field_names = fields.iter().map(|field| {
+        field
+            .ident
+            .as_ref()
+            .expect("Named fields should have an ident")
+    });
+
+    let output = quote! {
+        impl ::std::fmt::Debug for #struct_ident {
+          fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+              f.debug_struct(stringify!(#struct_ident))
+                  #(.field(stringify!(#field_names), &self.#field_names))*
+                  .finish()
+          }
+        }
+    };
+
     proc_macro::TokenStream::from(output)
 }
