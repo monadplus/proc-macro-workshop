28c3f26a3138422128e76806442f8ccc9e91fc33
Arnau Abella <arnauabella@gmail.com>
2023-01-30 18:10:24 +0100

    sorted/01-parse-enum

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,15 @@
-use proc_macro::TokenStream;
+use quote::ToTokens;
+use syn::parse_macro_input;
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn sorted(
+    args: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
     let _ = args;
-    let _ = input;
+    let item = parse_macro_input!(input as syn::Item);
 
-    unimplemented!()
+    let output = item.to_token_stream();
+
+    proc_macro::TokenStream::from(output)
 }
