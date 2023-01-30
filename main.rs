38f24ade2d4aae44c9e69e5a483d64a5ed39a268
Arnau Abella <arnauabella@gmail.com>
2023-01-30 10:28:20 +0100

    Clean main.rs

--- main.rs
+++ main.rs
@@ -1,14 +1,3 @@
-use seq::seq;
-
-seq!(N in 0..16 {
-    #[derive(Copy, Clone, PartialEq, Debug)]
-    enum Interrupt {
-        #(
-            Irq~N,
-        )*
-    }
-});
-
 // Write code here.
 //
 // To see what the code looks like after macro expansion:
