a70fc573193239223d140732398cff259833eb2b
Arnau Abella <arnauabella@gmail.com>
2023-01-31 08:43:24 +0100

    sorted/02-not-enum

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -6,10 +6,30 @@ pub fn sorted(
     args: proc_macro::TokenStream,
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let _ = args;
-    let item = parse_macro_input!(input as syn::Item);
+    let args = parse_macro_input!(args as proc_macro2::TokenStream);
+    let input = parse_macro_input!(input as syn::Item);
 
-    let output = item.to_token_stream();
+    match __sorted(args, input) {
+        Ok(output) => proc_macro::TokenStream::from(output),
+        Err(err) => err.into_compile_error().into(),
+    }
+}
 
-    proc_macro::TokenStream::from(output)
+fn __sorted(
+    args: proc_macro2::TokenStream,
+    input: syn::Item,
+) -> Result<proc_macro2::TokenStream, syn::Error> {
+    let ienum = match input {
+        syn::Item::Enum(ienum) => ienum,
+        _ => {
+            return Err(syn::Error::new_spanned(
+                args,
+                "expected enum or match expression",
+            ))
+        }
+    };
+
+    let output: proc_macro2::TokenStream = ienum.to_token_stream();
+
+    Ok(output)
 }
