6b61a36d4758e7ca903723e53efb8badbc0fa70e
Arnau Abella <arnauabella@gmail.com>
2023-01-31 09:21:26 +0100

    sorted/04-variants-with-data

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,18 +1,19 @@
+use proc_macro::TokenStream;
 use quote::ToTokens;
 use syn::parse_macro_input;
 
 #[proc_macro_attribute]
-pub fn sorted(
-    args: proc_macro::TokenStream,
-    input: proc_macro::TokenStream,
-) -> proc_macro::TokenStream {
+pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let args = parse_macro_input!(args as proc_macro2::TokenStream);
+    let mut output = input.clone();
+
     let input = parse_macro_input!(input as syn::Item);
 
-    match __sorted(args, input) {
-        Ok(output) => proc_macro::TokenStream::from(output),
-        Err(err) => err.into_compile_error().into(),
+    if let Err(err) = __sorted(args, input) {
+        let err = err.into_compile_error().into();
+        output.extend::<TokenStream>(err);
     }
+    output
 }
 
 fn __sorted(
@@ -33,7 +34,7 @@ fn __sorted(
         for v2 in ienum.variants.iter().skip(i + 1) {
             if v1.ident > v2.ident {
                 return Err(syn::Error::new_spanned(
-                    v2,
+                    &v2.ident,
                     format!("{} should sort before {}", v2.ident, v1.ident),
                 ));
             }
