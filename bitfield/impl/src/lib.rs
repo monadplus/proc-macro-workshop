2e4b6c2d9cae2ad4070ad42a7b49021afb7d844b
Arnau Abella <arnauabella@gmail.com>
2023-04-13 22:05:14 +0200

    bitfield/02-storage

--- bitfield/impl/src/lib.rs
+++ bitfield/impl/src/lib.rs
@@ -1,21 +1,60 @@
-use proc_macro::TokenStream;
-use proc_macro2::Span;
-use quote::quote;
+use std::fmt::Display;
+
+use proc_macro2::{Span, TokenStream};
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, DeriveInput, Fields, Ident};
+
+#[allow(dead_code)]
+fn error<U: Display, T: ToTokens>(message: U, tokens: T) -> proc_macro::TokenStream {
+    syn::Error::new_spanned(tokens, message)
+        .into_compile_error()
+        .into()
+}
 
 #[proc_macro_attribute]
-pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
+pub fn bitfield(
+    _args: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let DeriveInput {
+        attrs: _,
+        vis,
+        ident: struct_ident,
+        generics,
+        data,
+    } = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let fields = match data {
+        syn::Data::Struct(strct) => {
+            if let Fields::Named(fields) = strct.fields {
+                fields.named
+            } else {
+                unimplemented!("#[bitfield] must be used on a named struct");
+            }
+        }
+        _ => unimplemented!("#[bitfield] must be used on a struct"),
+    };
+
+    let tys = fields.into_iter().map(|field| field.ty);
+
+    let size = quote!((#(<#tys as Specifier>::BITS)+*) / 8);
+
+    let output = quote! {
+        #[repr(C)]
+        #vis struct #struct_ident #generics {
+            data: [u8; #size],
+        }
+    };
+
+    proc_macro::TokenStream::from(output)
 }
 
 #[proc_macro]
 pub fn generate_specifiers(_input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let mut output = proc_macro2::TokenStream::new();
+    let mut output = TokenStream::new();
 
     let specifiers = (1..=64).map(|index: usize| {
-        let s_ident = syn::Ident::new(&format!("B{}", index), Span::call_site());
+        let s_ident = Ident::new(&format!("B{}", index), Span::call_site());
         quote! {
             pub enum #s_ident {}
 
@@ -26,5 +65,5 @@ pub fn generate_specifiers(_input: proc_macro::TokenStream) -> proc_macro::Token
     });
     output.extend(specifiers);
 
-    output.into()
+    proc_macro::TokenStream::from(output)
 }
