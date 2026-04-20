2cb0fdadb4cbdd2c316d2e325dbcf59816453626
Arnau Abella <arnauabella@gmail.com>
2026-04-20 09:14:58 +0200

    builder/02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,53 @@
-use syn::DeriveInput;
+use quote::{format_ident, quote, quote_spanned};
+use syn::{spanned::Spanned, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let _input = syn::parse_macro_input!(input as DeriveInput);
-    let output = quote::quote! {
-        const {}
+    let input = syn::parse_macro_input!(input as DeriveInput);
+
+    let struct_name = input.ident;
+
+    let fields = match input.data {
+        syn::Data::Struct(s) => match s.fields {
+            syn::Fields::Named(fields) => fields,
+            _ => unimplemented!("Builder macro requires named fields structs"),
+        },
+        other => unimplemented!("Builder macro does not support {}", other),
     };
+
+    let builder_fields = fields.named.iter().map(|field| {
+        let name = &field.ident;
+        let ty = &field.ty;
+        quote_spanned! {field.span()=>
+            #name : Option<#ty>
+        }
+    });
+
+    let builder_name = format_ident!("{}Builder", struct_name);
+    let builder_struct = quote! {
+        pub struct #builder_name {
+            #(#builder_fields),*
+        }
+    };
+
+    let default_builder_fields = fields.named.iter().map(|field| {
+        let field_name = &field.ident;
+        quote_spanned! {field.span()=>
+            #field_name : None
+        }
+    });
+
+    let output = quote! {
+        #builder_struct
+
+        impl #struct_name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#default_builder_fields),*
+                }
+            }
+        }
+    };
+
     proc_macro::TokenStream::from(output)
 }
