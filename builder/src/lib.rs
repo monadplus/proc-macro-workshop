11cc3fbad1a968a1b9aa8550ed3a068ace2c065b
Arnau Abella <arnauabella@gmail.com>
2026-04-23 22:13:14 +0200

    builder/06

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,11 +1,15 @@
 use quote::{format_ident, quote, quote_spanned};
-use syn::{spanned::Spanned, DeriveInput};
+use syn::{
+    AngleBracketedGenericArguments, DeriveInput, GenericArgument, PathArguments, Type, TypePath,
+    spanned::Spanned,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = syn::parse_macro_input!(input as DeriveInput);
 
     let struct_name = input.ident;
+    let builder_name = format_ident!("{}Builder", struct_name);
 
     let fields = match input.data {
         syn::Data::Struct(s) => match s.fields {
@@ -15,10 +19,9 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         other => unimplemented!("Builder macro does not support {:?}", other),
     };
 
-    let builder_name = format_ident!("{}Builder", struct_name);
     let builder_fields = fields.named.iter().map(|field| {
         let name = &field.ident;
-        let ty = &field.ty;
+        let ty = inner_type(&field.ty, "Option").unwrap_or(&field.ty);
         quote_spanned! {field.span()=>
             #name : ::std::option::Option<#ty>
         }
@@ -33,7 +36,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
     let setter_fns = fields.named.iter().map(|field| {
         let name = &field.ident;
-        let ty = &field.ty;
+        let ty = inner_type(&field.ty, "Option").unwrap_or(&field.ty);
         quote! {
             pub fn #name (&mut self, #name: #ty) -> &mut Self {
                 self.#name = ::std::option::Option::Some(#name);
@@ -42,22 +45,18 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         }
     });
 
-    let build_fn = {
-        let field_assigns = fields.named.iter().map(|field| {
-            let name = &field.ident;
+    let build_fields_assignments = fields.named.iter().map(|field| {
+        let name = &field.ident;
+        if inner_type(&field.ty, "Option").is_some() {
+            quote! {
+                #name: self.#name.take()
+            }
+        } else {
             quote! {
                 #name: self.#name.take().ok_or_else(|| format!("{} not set", stringify!(#name)))?
             }
-        });
-
-        quote! {
-            pub fn build(&mut self) -> ::std::result::Result<#struct_name, ::std::boxed::Box<dyn ::std::error::Error>> {
-                Ok(#struct_name {
-                    #(#field_assigns),*
-                })
-            }
         }
-    };
+    });
 
     let output = quote! {
         pub struct #builder_name {
@@ -65,8 +64,13 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         }
 
         impl #builder_name {
-            #build_fn
             #(#setter_fns)*
+
+            pub fn build(&mut self) -> ::std::result::Result<#struct_name, ::std::boxed::Box<dyn ::std::error::Error>> {
+                Ok(#struct_name {
+                    #(#build_fields_assignments),*
+                })
+            }
         }
 
         impl #struct_name {
@@ -81,4 +85,26 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     // eprintln!("{}", output);
 
     proc_macro::TokenStream::from(output)
+}
+
+fn inner_type<'a>(ty: &'a Type, expected: &str) -> Option<&'a Type> {
+    if let Type::Path(TypePath { qself: _, path }) = ty {
+        if path.segments.len() != 1 || path.segments[0].ident != expected {
+            return None;
+        }
+
+        if let PathArguments::AngleBracketed(AngleBracketedGenericArguments {
+            colon2_token: _,
+            lt_token: _,
+            ref args,
+            gt_token: _,
+        }) = path.segments[0].arguments
+            && args.len() == 1
+            && let GenericArgument::Type(ref inner_type) = args[0]
+        {
+            return Some(inner_type);
+        }
+    }
+
+    None
 }
