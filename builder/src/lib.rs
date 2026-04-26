fbb6cf2ed6f980c92b2afa016a0717ffe613b9eb
Arnau Abella <arnauabella@gmail.com>
2026-04-26 19:39:25 +0200

    builder/07

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,13 +1,19 @@
-use quote::{format_ident, quote, quote_spanned};
+use proc_macro2::{Span, TokenStream};
+use quote::{format_ident, quote};
 use syn::{
-    AngleBracketedGenericArguments, DeriveInput, GenericArgument, PathArguments, Type, TypePath,
-    spanned::Spanned,
+    AngleBracketedGenericArguments, DeriveInput, Field, GenericArgument, Ident, LitStr,
+    PathArguments, Type, TypePath,
 };
 
-#[proc_macro_derive(Builder)]
-pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+#[proc_macro_derive(Builder, attributes(builder))]
+pub fn derive_builder(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = syn::parse_macro_input!(input as DeriveInput);
+    let output = derive(input).unwrap_or_else(syn::Error::into_compile_error);
+    // eprintln!("{}", output);
+    proc_macro::TokenStream::from(output)
+}
 
+fn derive(input: DeriveInput) -> syn::Result<TokenStream> {
     let struct_name = input.ident;
     let builder_name = format_ident!("{}Builder", struct_name);
 
@@ -21,35 +27,92 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
     let builder_fields = fields.named.iter().map(|field| {
         let name = &field.ident;
+
+        if inner_type(&field.ty, "Vec").is_some() {
+            let ty = &field.ty;
+            return quote! {
+                #name : #ty
+            };
+        }
+
         let ty = inner_type(&field.ty, "Option").unwrap_or(&field.ty);
-        quote_spanned! {field.span()=>
+        quote! {
             #name : ::std::option::Option<#ty>
         }
     });
 
     let default_builder_fields = fields.named.iter().map(|field| {
         let name = &field.ident;
+
+        if inner_type(&field.ty, "Vec").is_some() {
+            return quote! {
+                #name : ::std::vec::Vec::new()
+            };
+        }
+
         quote! {
             #name : ::std::option::Option::None
         }
     });
 
-    let setter_fns = fields.named.iter().map(|field| {
-        let name = &field.ident;
-        let ty = inner_type(&field.ty, "Option").unwrap_or(&field.ty);
-        quote! {
-            pub fn #name (&mut self, #name: #ty) -> &mut Self {
-                self.#name = ::std::option::Option::Some(#name);
-                self
+    let setter_fns = fields
+        .named
+        .iter()
+        .map(|field| {
+            let name = &field.ident;
+            let outer_ty = &field.ty;
+
+            if let Some(inner_ty) = inner_type(&field.ty, "Vec") {
+                let mut all_in_one_setter = quote! {
+                    pub fn #name (&mut self, #name: #outer_ty) -> &mut Self {
+                        self.#name = #name;
+                        self
+                    }
+                };
+
+                let each_setter = builder_attr(&field)?.map(|setter_name| {
+                    let setter_name = Ident::new(&setter_name, Span::call_site());
+                    // If the new one-at-a-time builder method is given the same name as the field,
+                    // avoid generating an all-at-once builder method for that field because the
+                    // names would conflict.
+                    if name.as_ref().is_some_and(|name| name == &setter_name) {
+                        all_in_one_setter = quote!();
+                    }
+
+                    quote! {
+                        pub fn #setter_name (&mut self, #name: #inner_ty) -> &mut Self {
+                            self.#name.push(#name);
+                            self
+                        }
+                    }
+                });
+
+                return Ok(quote! {
+                    #all_in_one_setter
+
+                    #each_setter
+                });
             }
-        }
-    });
+
+            let ty = inner_type(&field.ty, "Option").unwrap_or(outer_ty);
+            Ok(quote! {
+                pub fn #name (&mut self, #name: #ty) -> &mut Self {
+                    self.#name = ::std::option::Option::Some(#name);
+                    self
+                }
+            })
+        })
+        .collect::<Result<Vec<_>, syn::Error>>()?;
 
     let build_fields_assignments = fields.named.iter().map(|field| {
         let name = &field.ident;
         if inner_type(&field.ty, "Option").is_some() {
             quote! {
                 #name: self.#name.take()
+            }
+        } else if inner_type(&field.ty, "Vec").is_some() {
+            quote! {
+                #name: self.#name.drain(..).collect::<Vec<_>>()
             }
         } else {
             quote! {
@@ -82,9 +145,30 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         }
     };
 
-    // eprintln!("{}", output);
+    Ok(output)
+}
 
-    proc_macro::TokenStream::from(output)
+fn builder_attr<'a>(field: &Field) -> Result<Option<String>, syn::Error> {
+    let mut each_value = None;
+
+    for attr in &field.attrs {
+        if !attr.path().is_ident("builder") {
+            continue;
+        }
+
+        attr.parse_nested_meta(|meta| {
+            if meta.path.is_ident("each") {
+                let value = meta.value()?;
+                let lit: LitStr = value.parse()?;
+                each_value = Some(lit.value());
+                return Ok(());
+            }
+
+            Err(meta.error("expected #[builder(each = \"...\")]"))
+        })?
+    }
+
+    Ok(each_value)
 }
 
 fn inner_type<'a>(ty: &'a Type, expected: &str) -> Option<&'a Type> {
