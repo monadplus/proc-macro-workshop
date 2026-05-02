d0529515147a2531a22a0d7f395ef3e4fedaf129
Arnau Abella <arnauabella@gmail.com>
2026-05-02 19:40:43 +0200

    debug/01

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,17 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::DeriveInput;
 
 #[proc_macro_derive(CustomDebug)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn derive_debug(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = syn::parse_macro_input!(input as DeriveInput);
+    let output = derive(input).unwrap_or_else(syn::Error::into_compile_error);
+    // eprintln!("{}", output);
+    proc_macro::TokenStream::from(output)
+}
 
-    unimplemented!()
+fn derive(_input: DeriveInput) -> syn::Result<TokenStream> {
+    let output = quote! {};
+
+    Ok(output)
 }
