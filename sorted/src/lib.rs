20e9862f88a7a90f8d7d17810498f3b175eb05d3
Arnau Abella <arnauabella@gmail.com>
2026-05-18 22:37:09 +0200

    sorted/02

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,5 +1,6 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{Span, TokenStream};
 use quote::ToTokens;
+use syn::Item;
 
 #[proc_macro_attribute]
 pub fn sorted(
@@ -9,13 +10,20 @@ pub fn sorted(
     let _ = args;
     let input = syn::parse_macro_input!(input as syn::Item);
 
-    let output = __sorted(input).unwrap_or_else(syn::Error::into_compile_error);
+    let output = sorted_enum(input).unwrap_or_else(syn::Error::into_compile_error);
     // eprintln!("{}", output);
     // panic!("{}", output);
 
     proc_macro::TokenStream::from(output)
 }
 
-fn __sorted(item: syn::Item) -> syn::Result<TokenStream> {
-    Ok(item.into_token_stream())
+fn sorted_enum(item: Item) -> syn::Result<TokenStream> {
+    let Item::Enum(item_enum) = item else {
+        return Err(syn::Error::new(
+            Span::call_site(),
+            "expected enum or match expression",
+        ));
+    };
+
+    Ok(item_enum.into_token_stream())
 }
