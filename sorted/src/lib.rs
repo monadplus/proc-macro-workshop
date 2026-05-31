45c6763b1d368d8b634619a3ef37b8079538941f
Arnau Abella <arnauabella@gmail.com>
2026-05-31 19:35:20 +0200

    sorted/05

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,11 +1,12 @@
 use proc_macro2::Span;
+use quote::quote;
+use syn::visit_mut::VisitMut;
 
 #[proc_macro_attribute]
 pub fn sorted(
-    args: proc_macro::TokenStream,
+    _args: proc_macro::TokenStream,
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let _ = args;
     let mut output = input.clone();
     let enum_item = syn::parse_macro_input!(input as syn::Item);
 
@@ -27,19 +28,85 @@ fn sorted_enum(item: syn::Item) -> Option<syn::Error> {
         ));
     };
 
-    let variants = item_enum.variants.iter().collect::<Vec<_>>();
-    for i in 1..variants.len() {
-        let current = &variants[i].ident;
-        let prev = &variants[i - 1].ident;
+    let variants = item_enum
+        .variants
+        .into_iter()
+        .map(|v| v.ident)
+        .collect::<Vec<_>>();
+
+    find_out_of_order(variants)
+}
+
+#[proc_macro_attribute]
+pub fn check(
+    _args: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let mut fn_item = syn::parse_macro_input!(input as syn::ItemFn);
+
+    let mut visitor = MatchSorted::default();
+    visitor.visit_item_fn_mut(&mut fn_item);
+    let mut output = proc_macro::TokenStream::from(quote! {#fn_item});
+
+    for error in visitor.errors {
+        output.extend(proc_macro::TokenStream::from(error.into_compile_error()));
+    }
+
+    // eprintln!("{}", output);
+    // panic!("{}", output);
+    output
+}
+
+#[derive(Default)]
+struct MatchSorted {
+    pub errors: Vec<syn::Error>,
+}
+
+impl VisitMut for MatchSorted {
+    fn visit_expr_match_mut(&mut self, node: &mut syn::ExprMatch) {
+        if let Some(idx) = node
+            .attrs
+            .iter()
+            .position(|attr| attr.path().is_ident("sorted"))
+        {
+            // Strip to avoid a compilation error
+            node.attrs.remove(idx);
+
+            let variants = node
+                .arms
+                .iter()
+                .filter_map(|arm| match &arm.pat {
+                    syn::Pat::Struct(v) => v.path.segments.last().map(|p| p.ident.clone()),
+                    syn::Pat::TupleStruct(v) => v.path.segments.last().map(|p| p.ident.clone()),
+                    otherwise => {
+                        let err = syn::Error::new_spanned(otherwise, r#"unsupported by #[sorted]"#);
+                        self.errors.push(err);
+                        None
+                    }
+                })
+                .collect::<Vec<_>>();
+
+            if let Some(err) = find_out_of_order(variants) {
+                self.errors.push(err);
+            }
+        }
+    }
+}
+
+fn find_out_of_order<T>(vs: Vec<T>) -> Option<syn::Error>
+where
+    T: Ord + syn::spanned::Spanned + std::fmt::Display,
+{
+    for i in 1..vs.len() {
+        let current = &vs[i];
+        let prev = &vs[i - 1];
 
         if current < prev {
-            let before = variants[..i]
-                .binary_search_by(|variant| variant.ident.cmp(&current))
-                .unwrap_err();
-            let before = &variants[before].ident;
+            let before = vs[..i].binary_search_by(|v| v.cmp(&current)).unwrap_err();
+            let before = &vs[before];
 
             return Some(syn::Error::new(
-                variants[i].ident.span(),
+                current.span(),
                 format!("{} should sort before {}", current, before),
             ));
         }
